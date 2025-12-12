12a89aa9e119cdd1232de90d16c649581e4d946a
arnesmila56-commits <arnesmila56@gmail.com>
2025-12-12 09:18:40 -0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Georgia, serif" }}>
+      <h1>Premier RTA Cabinetry</h1>
+      <p>The site is live. Full version will be restored next.</p>
+    </div>
+  );
+}
+
+export default App;
